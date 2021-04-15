4d5f439a79eec7113299b092ee2a035854306483
Nana Kofi Aferi <githubaferi@gmail.com>
2021-04-15 19:11:41 +0000

    chore(Queue):Implemented a Queue data structure.

--- Queue.playground/Contents.swift
+++ Queue.playground/Contents.swift
@@ -1,3 +1,51 @@
 import UIKit
 
-var str = "Hello, playground"
+struct Queue {
+    var arr:[Int] = []
+    
+    var head: Int? {
+        return arr.first
+    }
+    
+    var tail: Int? {
+        return arr.last
+    }
+    
+    
+    mutating func enQueue(_ item: Int) {
+        return arr.append(item)
+    }
+    
+    mutating func deQueue() -> Int? {
+        guard !isEmpty() else {
+            return nil
+        }
+        let res = arr.first
+        arr.removeFirst()
+        return res
+        
+    }
+    
+    
+    mutating func isEmpty() -> Bool {
+        guard arr.count <= 0 else {
+            return false
+        }
+        return true
+    }
+}
+
+//*****Usage of a Queue*****
+var queue = Queue()
+queue.enQueue(1)
+queue.enQueue(2)
+queue.enQueue(3)
+print(queue.arr)
+print(queue.isEmpty())
+let x = queue.deQueue()
+print("DeQueued : ", x ?? -1)
+print(queue.arr)
+queue.enQueue(4)
+print(queue.arr)
+print(queue.head)
+print(queue.tail)
